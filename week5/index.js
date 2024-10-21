710e7c137f57e88221694b0b77062baaf76f0a80
Bella_Ordonez <iordonez@scu.edu>
2024-10-21 11:32:15 -0700

    wrote some javascript

---
+++ week5/index.js
@@ -0,0 +1,22 @@
+// console.log("hello world")
+// alert('Hello, world!');
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+// document.append('<p>This should come after the h1</p>')
+
+
+/*
+  Function to calculate the sum of two numbers.
+  Inputs: a, b - numbers to be added.
+  Output: returns the sum of a and b.
+*/
+
+/**  
+ * @adescription this funtionadds two 
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @return {number} The sum of a and b. 
+ * */
+function add(a, b) {
+    return a + b;
+}
