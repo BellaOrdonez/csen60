250fa00cdc318751f71834cc52f13b185365d3a3
Bella_Ordonez <iordonez@scu.edu>
2024-11-24 07:31:22 -0800

    create hono server

--- week8/index.js
+++ week8/index.js
@@ -9,16 +9,16 @@ async function getRandomFact() {
     const response = await fetch("https://uselessfacts.jsph.pl//api/v2/facts/random")
     // console.log(response)
     const data = await response.json()
-    // console.log(data.text)
-    console.log("getData finished")
+    console.log(data.text, "\n")
+    // console.log("getData finished")
 }
 
-async function getRandomFact() {
-    const response = await fetch("https://uselessfacts.jsph.pl//api/v2/facts/random")
+async function getTodaysFact() {
+    const response = await fetch("https://uselessfacts.jsph.pl//api/v2/facts/today")
     // console.log(response)
     const data = await response.json()
-    // console.log(data.text)
-    console.log("getData finished")
+    console.log(data.text, "\n")
+    // console.log("getData finished")
 }
 
 
